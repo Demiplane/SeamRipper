ccba5aa2905596211fee3b7bf44a35da008cc9a4
Isaac Zais <isaaczais@hotmail.com>
2018-04-10 10:06:36 -0500

    Added minifier, stubbed new routes and views

--- SeamRipper/Controllers/DeckController.cs
+++ SeamRipper/Controllers/DeckController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SeamRipper.Models;
 
 namespace SeamRipper.Controllers
@@ -15,6 +16,23 @@ namespace SeamRipper.Controllers
             var deckViewModel = new DeckViewModel(Deck.CreateFullDeck());
 
             return View("DeckView", deckViewModel);
+        }
+
+        [Route("/deck/declare/{drawCount}")]
+        public IActionResult Declare(int drawCount)
+        {
+            return new JsonResult(JsonConvert.SerializeObject(drawCount));
+        }
+
+        [Route("/deck/draw/{drawCount}")]
+        public IActionResult Draw(int drawCount)
+        {
+            return new JsonResult(JsonConvert.SerializeObject(drawCount));
+        }
+
+        public IActionResult FreeDraw()
+        {
+            return Ok();
         }
     }
 }
